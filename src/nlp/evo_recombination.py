52fb4dced5ccaa84535e8939ed9ed22feb47d63d
Sean Takafuji <seanytak@hawaii.edu>
2018-12-15 11:37:34 -1000

    Added recombination code

--- src/nlp/evo_recombination.py
+++ src/nlp/evo_recombination.py
@@ -7,13 +7,24 @@ import language_check
 import numpy as np
 import pandas as pd
 
-# def recombine(nlp, population, recombine_func):
-#     """Mutates a member of the population using mutate_func with probability mutation_rate 
-#     """
-#     population_mut = []
-#     for member in population:
-#         offspring = member
-#         if rand.uniform(0, 1) <= mutation_rate:
-#             offspring = recombine_func(nlp, member)
-#         population_mut.append(offspring)
-#     return population_mut
+def recombine(nlp, population, recombine_func, recombination_rate=0.05):
+    """Mutates a member of the population using mutate_func with probability mutation_rate 
+    """
+    population_mut = []
+    for member in population:
+        offspring = member
+        if rand.uniform(0, 1) <= recombination_rate:
+            offspring = recombine_func(nlp, member, population[rand.randrange(0, len(population))])
+        population_mut.append(offspring)
+    return population_mut
+
+def recombine_words(nlp, parent_one: str, parent_two: str):
+    words_one = parent_one.split(' ')
+    words_two = parent_two.split(' ')
+    xo_one = rand.randrange(0, len(words_two))
+    xo_two = rand.randrange(0, len(words_two))
+    for i in range(min(xo_one, xo_two), max(xo_one, xo_two)):
+        words_one[i] = words_two[i]
+    return ' '.join(words_one)
+
+
